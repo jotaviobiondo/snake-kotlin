a1b60e15e2aca7d22eea30e48d4f28b28a24a14c
João Otávio Biondo <joao.o.b@hotmail.com>
2020-03-29 12:45:08 -0300

    refactoring Food entity to use Position internally

--- src/main/kotlin/io/github/jotaviobiondo/Food.kt
+++ src/main/kotlin/io/github/jotaviobiondo/Food.kt
@@ -2,7 +2,12 @@ package io.github.jotaviobiondo
 
 import kotlin.random.Random
 
-class Food(val x: Int, val y: Int) {
+class Food(x: Int, y: Int) {
+
+    val position: Position = Position(x, y)
+
+    val x get() = position.x
+    val y get() = position.y
 
     companion object {
         fun random(maxX: Int, maxY: Int): Food {
